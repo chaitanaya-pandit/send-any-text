26fafe634833bba2f3660c5b933a1487493e2a26
chaitanaya pandit <chaitanayapandit@gmail.com>
2026-02-09 23:08:59 +0530

    last message for backend

--- server/socket/index.js
+++ server/socket/index.js
@@ -4,13 +4,11 @@ module.exports=(io)=>{
 
         socket.on("joinroom",(path,cb)=>{
             socket.join(path);
-            console.log(`socket id:${socket.id} in this room ${path}`)
             cb("joined room")
         })
 
         socket.on("sendtext",({path,text})=>{
-            io.to(path).emit("recievetext",text)
-            console.log(`text:${text} send to room ${path}`)
+            io.to(path).emit("recievetext",text)  
         })
 
         socket.on("disconnect",(socket)=>{
